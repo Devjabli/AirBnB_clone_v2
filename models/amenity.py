8c8043e0876d8944b8f85e5814cde5ac999a7f1a
Devjabli <aroussiotmane27@gmail.com>
2024-06-27 02:48:02 +0100

    test

--- models/amenity.py
+++ models/amenity.py
@@ -3,20 +3,18 @@
 Module Amenity class.
 """
 from models.base_model import BaseModel, Base
-from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128),
-                      nullable=False)
-    else:
-        name = ""
+    """"Class that Define the Amenities"""
 
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+    __tablename__ = "amenities"
+    name = Column(String(128),
+                  nullable=False)
+
+    place_amenities = relationship("Place",
+                                   secondary='place_amenity',
+                                   viewonly=False)
