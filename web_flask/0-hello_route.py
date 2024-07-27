c30b554933a5552012f742b712046a6e5c9b042d
deV <aroussiotmane27@gmail.com>
2024-07-27 23:02:10 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script run flask web application and 
+This script run flask web application and
 listening on 0.0.0.0 port 5000
 """
 
@@ -12,7 +12,6 @@ app = Flask(__name__)
 def hello():
     """Return string"""
     return ("Hello HBNB!")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
