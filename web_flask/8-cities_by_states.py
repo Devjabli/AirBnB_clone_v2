5442d576b446714e2ab97ca8321f7928a90f8904
deV <aroussiotmane27@gmail.com>
2024-07-28 00:50:06 +0100

    full latest tasks

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Importing Flask to run the web app"""
 from flask import Flask, render_template
-from models.engine.db_storage import storage
+from models.engine.file_storage import storage
 from models.state import State
 
 
